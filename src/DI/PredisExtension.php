3c97c757cedf5a3cc9839c37b8b5e4c1b00a9753
Marek Bartoš <bartos.developer152@gmail.com>
2020-12-22 10:54:37 +0100

    Initial commit

---
+++ src/DI/PredisExtension.php
@@ -0,0 +1,133 @@
+<?php declare(strict_types = 1);
+
+namespace OriNette\Predis\DI;
+
+use Nette\Bridges\HttpDI\SessionExtension;
+use Nette\DI\CompilerExtension;
+use Nette\DI\Definitions\ServiceDefinition;
+use Nette\Http\Session;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
+use OriNette\Predis\SessionHandler;
+use Orisai\Exceptions\Logic\InvalidArgument;
+use Orisai\Exceptions\Logic\InvalidState;
+use Predis\Client;
+use stdClass;
+use function assert;
+
+/**
+ * @property-read stdClass $config
+ */
+final class PredisExtension extends CompilerExtension
+{
+
+	public function getConfigSchema(): Schema
+	{
+		return Expect::structure([
+			'connections' => Expect::arrayOf(
+				Expect::structure([
+					'parameters' => Expect::array()->default([]),
+					'options' => Expect::array()->default([]),
+					'autowired' => Expect::bool()->default(false),
+				]),
+			),
+			'session' => Expect::anyOf(
+				Expect::structure([
+					'connection' => Expect::string()->required(),
+					'sessionTtl' => Expect::anyOf(Expect::int(), Expect::null()),
+					'lockTtl' => Expect::anyOf(Expect::int(), Expect::null()),
+				]),
+				Expect::null(),
+			),
+		]);
+	}
+
+	public function loadConfiguration(): void
+	{
+		parent::loadConfiguration();
+
+		$builder = $this->getContainerBuilder();
+		$config = $this->config;
+
+		foreach ($config->connections as $connectionKey => $connectionConfig) {
+			$builder->addDefinition($this->formatConnectionName($connectionKey))
+				->setFactory(Client::class, [
+					$connectionConfig->parameters,
+					$connectionConfig->options,
+				])
+				->setAutowired($connectionConfig->autowired);
+		}
+
+		$this->loadConfigurationSession($config->session);
+	}
+
+	public function beforeCompile(): void
+	{
+		parent::beforeCompile();
+
+		$config = $this->config;
+
+		$this->beforeCompileSession($config->session);
+	}
+
+	private function loadConfigurationSession(?stdClass $sessionConfig): void
+	{
+		if ($sessionConfig === null) {
+			return;
+		}
+
+		$builder = $this->getContainerBuilder();
+
+		$sessionConnectionKey = $this->prefix('session.connection');
+		$sessionConnectionName = $this->formatConnectionName($sessionConfig->connection);
+
+		if (!$builder->hasDefinition($sessionConnectionName)) {
+			throw InvalidArgument::create()
+				->withMessage(
+					"{$sessionConnectionKey} with value {$sessionConfig->connection} requires {$sessionConnectionName} to be configured.",
+				);
+		}
+
+		$builder->addDefinition($this->prefix('sessionHandler'))
+			->setFactory(SessionHandler::class, [
+				$builder->getDefinition($sessionConnectionName),
+				$sessionConfig->sessionTtl,
+				$sessionConfig->lockTtl,
+			])
+			->setAutowired(false);
+	}
+
+	private function beforeCompileSession(?stdClass $sessionConfig): void
+	{
+		if ($sessionConfig === null) {
+			return;
+		}
+
+		$builder = $this->getContainerBuilder();
+
+		$sessionConfigKey = $this->prefix('session');
+
+		if ($builder->getByType(Session::class) === null) {
+			$sessionClass = Session::class;
+			$sessionExtensionClass = SessionExtension::class;
+
+			throw InvalidState::create()
+				->withMessage(
+					"{$sessionClass} must be available in DI to configure {$sessionConfigKey}. Install nette/http and register {$sessionExtensionClass}.",
+				);
+		}
+
+		$sessionDef = $builder->getDefinitionByType(Session::class);
+		assert($sessionDef instanceof ServiceDefinition);
+
+		$sessionDef->addSetup('setHandler', [
+			$builder->getDefinition($this->prefix('sessionHandler')),
+		]);
+	}
+
+	private function formatConnectionName(string $key): string
+	{
+		return $this->prefix("connection.{$key}");
+	}
+
+}
---
+++ src/SessionHandler.php
@@ -0,0 +1,158 @@
+<?php declare(strict_types = 1);
+
+namespace OriNette\Predis;
+
+use Predis\Client;
+use SessionHandlerInterface;
+use Symfony\Component\Lock\LockFactory;
+use Symfony\Component\Lock\LockInterface;
+use Symfony\Component\Lock\Store\RedisStore;
+use function ini_get;
+
+final class SessionHandler implements SessionHandlerInterface
+{
+
+	private Client $client;
+	private LockFactory $lockFactory;
+
+	private int $sessionTtl;
+	private int $lockTtl;
+
+	/** @var array<string, LockInterface> */
+	private array $locks = [];
+
+	public function __construct(Client $client, ?int $sessionTtl = null, ?int $lockTtl = null)
+	{
+		$this->sessionTtl = $this->getTtl($sessionTtl, 'session.gc_maxlifetime', 86_400);
+		$this->lockTtl = $this->getTtl($lockTtl, 'max_execution_time', 30);
+
+		$this->client = $client;
+		$this->lockFactory = new LockFactory(new RedisStore($client, $this->lockTtl));
+	}
+
+	private function getTtl(?int $givenTtl, string $iniOptionName, int $default): int
+	{
+		if ($givenTtl !== null) {
+			return $givenTtl;
+		}
+
+		$iniOption = ini_get($iniOptionName);
+
+		return $iniOption !== ''
+			? (int) $iniOption
+			: $default;
+	}
+
+	/**
+	 * @param string $save_path
+	 * @param string $name
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+	 */
+	public function open($save_path, $name): bool
+	{
+		if (!$this->client->isConnected()) {
+			$this->client->connect();
+		}
+
+		return true;
+	}
+
+	public function close(): bool
+	{
+		foreach ($this->locks as $lock) {
+			$lock->release();
+		}
+
+		$this->locks = [];
+
+		return true;
+	}
+
+	/**
+	 * @param string $session_id
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+	 */
+	public function destroy($session_id): bool
+	{
+		$this->client->del(
+			$this->prefix($session_id),
+		);
+
+		$this->releaseLock($session_id);
+
+		return true;
+	}
+
+	/**
+	 * @param int $maxlifetime
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+	 */
+	public function gc($maxlifetime): bool
+	{
+		return true;
+	}
+
+	/**
+	 * @param string $session_id
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+	 */
+	public function read($session_id): string
+	{
+		$this->acquireLock($session_id);
+
+		$data = $this->client->get(
+			$this->prefix($session_id),
+		);
+
+		if ($data === null) {
+			return '';
+		}
+
+		return $data;
+	}
+
+	/**
+	 * @param string $session_id
+	 * @param string $session_data
+	 * @phpcsSuppress SlevomatCodingStandard.TypeHints.ParameterTypeHint.MissingNativeTypeHint
+	 */
+	public function write($session_id, $session_data): bool
+	{
+		$this->client->setex(
+			$this->prefix($session_id),
+			$this->sessionTtl,
+			$session_data,
+		);
+
+		return true;
+	}
+
+	private function prefix(string $session_id): string
+	{
+		return "orisai.session.$session_id";
+	}
+
+	private function acquireLock(string $session_id): void
+	{
+		$lock = $this->locks[$session_id]
+			?? ($this->locks[$session_id] = $this->lockFactory->createLock($session_id, $this->lockTtl));
+
+		if (!$lock->isAcquired()) {
+			$lock->acquire(true);
+		}
+	}
+
+	private function releaseLock(string $session_id): void
+	{
+		if (!isset($this->locks[$session_id])) {
+			return;
+		}
+
+		$lock = $this->locks[$session_id];
+
+		$lock->release();
+
+		unset($this->locks[$session_id]);
+	}
+
+}
